2c7eed562974fc042037531c2883e3da685e4875
Sirlam <mdelosab@gmail.com>
2022-04-19 16:49:01 -0300

    draw bubbles

--- bubbles.js
+++ bubbles.js
@@ -10,8 +10,8 @@ let fps = 15;
 let ang = 0;
 
 let brickRowCount = 5;
-let brickColumnCount = 10;
-let brickPadding = 10;
+let brickColumnCount = 20;
+let brickPadding = 12;
 let brickOffsetTop = 30;
 let brickOffsetLeft = 30;
 
@@ -19,16 +19,14 @@ let bubbles = [];
 for(c=0; c<brickColumnCount; c++) {
     bubbles[c] = [];
     for(r=0; r<brickRowCount; r++) {
-        bubbles[c][r] = { x: 0, y: 0, status: 1 };
-        brickRowCount -= 2;
+        bubbles[c][r] = { x: 0, y: 0, status: 1, value: 1 };
     }
 }
-
+console.log(bubbles)
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = color;
-	ctx.Stroke="10"
     ctx.fill();
     ctx.closePath();
 }
@@ -37,26 +35,34 @@ function drawBubbles() {
     for(c=0; c<brickColumnCount; c++) {
         for(r=0; r<brickRowCount; r++) {
             if(bubbles[c][r].status == 1) {
-                let brickX = (c*ballRadius)+brickOffsetLeft;
-                let brickY = (r*ballRadius)+brickOffsetTop;
+
+                n = 0;
+                let bubble = bubbles[c][r];
+                
+
+                let brickX = (c*(ballRadius+brickPadding))+brickOffsetLeft;
+                let brickY = (r*(ballRadius+brickPadding))+brickOffsetTop;
                 bubbles[c][r].x = brickX;
                 bubbles[c][r].y = brickY;
-                console.log(brickX)
                 ctx.beginPath();
                 ctx.arc(brickX, brickY, ballRadius, 0, Math.PI*2);
                 ctx.fillStyle = color;
-	            ctx.Stroke="10"
+	            ctx.Stroke=10
                 ctx.fill();
                 ctx.closePath();
+                
             }
+            
         }
+        
     }
 }
 
+
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBubbles();
     drawBall();
 }
 
-setInterval(draw, fps);
+draw();
